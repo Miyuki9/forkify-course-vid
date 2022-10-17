735cb3843cd2a442c788da345283e6c6cff58ab1
Miyuki9 <57666124+Miyuki9@users.noreply.github.com>
2022-10-17 13:20:15 +0300

    Delete config.js

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10; //res==result
-export const KEY = "ed930c26-56b5-4cd1-b04e-4a43ef8e7651";
-export const MODAL_CLOSE_SEC = 2.5;
